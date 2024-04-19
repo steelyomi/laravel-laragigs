443269aaba38ea34d2aba590191197a1f5b8cbd3
steelyomi <bmlasisi@gmail.com>
2024-04-19 13:09:50 +0100

    Controllers/LayoutAsComponent

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Models\Listing;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Models\Listing;
+use App\Http\Controllers\ListingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,21 +17,11 @@ use App\Models\Listing;
 */
 
 // All Listings 
-Route::get('/', function () {
-    return view('listings', [
-        'heading' => 'Latest Listings',
-        'listings' => Listing::all()
-    ]);
-});
+Route::get('/', [ListingController::class, 'index']);
 
 // SIngle Listing
 
-Route::get('/listings/{listing}', function (Listing 
-$listing) {
-    return view('listing', [
-        'listing' => $listing
-    ]);
-});
+Route::get('/listings/{listing}', [ListingController::class, 'show']);
 
 
 
