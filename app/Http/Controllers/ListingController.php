b665a8eb2fb0a18801a249390f8ba3a2ff2431dd
steelyomi <bmlasisi@gmail.com>
2024-05-07 11:24:31 +0100

    deleteListing

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -75,4 +75,11 @@ class ListingController extends Controller
 
         return back()->with('message', 'Listing updated successfully!');
     }
+
+    // Delete listing
+
+    public function destroy(Listing $listing) {
+        $listing->delete();
+        return redirect('/')->with('message', 'Listing deleted successfully!');
+    }
 }
